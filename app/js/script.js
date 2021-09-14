4fe711e14eaa6856a0ea268449f5c92a0ae6fab1
Britt Ramsubick <bramsubic@gmail.com>
2021-09-14 12:21:27 -0400

    add pseudocode game log

---
+++ app/js/script.js
@@ -0,0 +1,39 @@
+/*----- constants -----*/
+// gameBoard array 
+
+/*----- app's state (variables) -----*/
+// score, speed, snakeBody, snakePosition, apple, applePosition 
+
+/*----- cached element references -----*/
+//select the board, score, and the start and restart button 
+
+/*----- event listeners -----*/
+// keycodes so when the key is pressed the direction changes (function executes)
+
+/*----- functions -----*/
+// keycode function 
+
+// start game and restart game 
+
+// randomly generate an apple 
+// make sure it doesnt appear where the snake is 
+Math.floor(Math.random() *)
+
+// snake outcomes 
+
+    // if snake eats apple 
+        // attach apple to snake body 
+        // score will increase by 1 
+        // display score on dom 
+        
+    // else snake hitting self 
+    // else snake hitting border  top, right, bottom, left 
+
+// what happens if snake hits anything other than an apple? 
+    // interval ends and score goes back to 0 
+    // snake loses all of the apples 
+    // direction of the snake restarts 
+
+    // what happens to the snake 
+// loses all of the apples from the tail 
+// direction of the snake restarts 
